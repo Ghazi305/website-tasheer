e3177a09cbb2ba89ab403262c44206f776f70e21
ghazi <ghaziesam13@gmail.com>
2025-08-29 11:54:56 +0300

    updated

--- client/src/app/ucsi-university/page.tsx
+++ client/src/app/ucsi-university/page.tsx
@@ -1,9 +1,8 @@
-'use client';
-
-import Footer from "@/components/Footer";
-import MultiStepForm from "@/components/forms/MultiStepForm";
+import dynamic from 'next/dynamic';
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
+const MultiStepForm = dynamic(() => import('@/components/forms/MultiStepForm'), { ssr: false });
 
 export default function UniversitiesPage() {
   return (
